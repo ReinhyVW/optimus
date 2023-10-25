4015ff1fa14a08eab2f154132daff771bc574c11
ReinhyVW <manu_cabarosa32@outlook.com>
2023-10-25 10:28:36 -0500

    Initial Commit 3.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-red-500">
+    <div className="w-screen h-screen bg-blue-500">
       <h1>Soon to come!</h1>
     </div>
   )
